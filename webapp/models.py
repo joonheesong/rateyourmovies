37c69f87adbf9176cc5a27cb9dd2830657ea8e31
Simon Joon-Hee Song <joonhee.song@gmail.com>
2016-11-23 15:39:06 -0500

    Inital commit for rateyourmovies app

---
+++ webapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class User(models.Model):
+	pass
+
+class Movie(models.Model):
+	pass
+
+class Post(models.Model):
+	pass
