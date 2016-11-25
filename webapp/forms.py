8ea3f7e11f4c510396bebc5cbd2b5289113ca22d
Simon Joon-Hee Song <joonhee.song@gmail.com>
2016-11-25 01:42:32 -0500

    Sign up page completed.

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,14 +3,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+	username = forms.CharField(
+							max_length=50,
+							widget=forms.TextInput(attrs={'class': "input__field input__field--chisato"}))
+	password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': "input__field input__field--chisato"}))
+	password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': "input__field input__field--chisato"}))
+	email = forms.EmailField(required=True,
+							widget=forms.TextInput(attrs={'class': "input__field input__field--chisato"}))
 
 	class Meta:
 		model = User
 		fields = {'username', 'email', 'password1', 'password2'}
 
 	def save(self, commit=True):
-		user = super(RegistraionForm, self).save(commit=False)
+		user = super(RegistrationForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
 
 		if commit:
